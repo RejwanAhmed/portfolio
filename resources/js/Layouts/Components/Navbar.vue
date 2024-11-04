9441fffec24d17bc2313a2c7b752c8ef8e1d0c28
Rejwan <rejwan.nonditosoft@gmail.com>
2024-11-05 01:47:38 +0600

    navbar designed

---
+++ resources/js/Layouts/Components/Navbar.vue
@@ -0,0 +1,42 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <Link class="navbar-brand" :href="route('dashboard')">RJ Portfolio</Link>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <Link class="nav-link active" aria-current="page" :href="route('dashboard')"><i class="bi bi-house-door me-1"></i>Dashboard</Link>
+                    </li>
+
+                </ul>
+                <div class="d-flex">
+                    <ul class="navbar-nav mb-2 mb-lg-0">
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                {{ $page.props.auth.user.name }}
+                            </a>
+                            <ul class="dropdown-menu dropdown-menu-end">
+                                <li>
+                                    <Link class="dropdown-item" :href="route('profile.edit')"><i class="bi bi-person-circle me-2"></i>Profile</Link>
+                                </li>
+                                <li>
+                                    <Link method="post" class="dropdown-item" :href="route('logout')"><i class="bi bi-box-arrow-right me-2"></i>Logout</Link>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </nav>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3';
+</script>
