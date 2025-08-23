22cb9f7489e51b32e6b612147689904ed5333383
RejwanAhmed <rejwancse10@gmail.com>
2025-08-23 19:52:20 +0600

    used move method instead of store as for file upload

--- app/Services/Core/HelperService.php
+++ app/Services/Core/HelperService.php
@@ -7,48 +7,86 @@ use Illuminate\Support\Facades\Storage;
 
 class HelperService
 {
+    // public static function uploadImage($image = null, $oldImagePath = null, $directory)
+    // {
+    //     if ($image instanceof \Illuminate\Http\UploadedFile) {
+    //         if($oldImagePath) {
+    //             $relativePath = str_replace('storage/', '', $oldImagePath);
+    //             if(Storage::disk('public')->exists($relativePath)) {
+    //                 Storage::disk('public')->delete($relativePath);
+    //             }
+    //         }
+
+    //         $imageName = uniqid().'.'.$image->getClientOriginalExtension();
+    //         $path = $image->storeAs($directory, $imageName, 'public');
+    //         return 'storage/' . $path;
+    //     }
+
+    //     return $oldImagePath;
+    // }
+
     public static function uploadImage($image = null, $oldImagePath = null, $directory)
     {
         if ($image instanceof \Illuminate\Http\UploadedFile) {
-            if($oldImagePath) {
-                $relativePath = str_replace('storage/', '', $oldImagePath);
-                if(Storage::disk('public')->exists($relativePath)) {
-                    Storage::disk('public')->delete($relativePath);
-                }
+            if($oldImagePath && File::exists(public_path($oldImagePath))) {
+                File::delete(public_path($oldImagePath));
             }
 
             $imageName = uniqid().'.'.$image->getClientOriginalExtension();
-            $path = $image->storeAs($directory, $imageName, 'public');
-            return 'storage/' . $path;
+            $image->move(public_path($directory), $imageName);
+            $imagePath = $directory.'/'.$imageName;
+            return $imagePath;
         }
 
         return $oldImagePath;
     }
 
+    // public static function deleteImage(string $imagePath)
+    // {
+    //     if ($imagePath) {
+    //         $relativePath = str_replace('storage/', '', $imagePath);
+    //          if (Storage::disk('public')->exists($relativePath)) {
+    //             Storage::disk('public')->delete($relativePath);
+    //         }
+    //     }
+    // }
+
     public static function deleteImage(string $imagePath)
     {
-        if ($imagePath) {
-            $relativePath = str_replace('storage/', '', $imagePath);
-             if (Storage::disk('public')->exists($relativePath)) {
-                Storage::disk('public')->delete($relativePath);
-            }
+        if ($imagePath && File::exists(public_path($imagePath))) {
+            File::delete(public_path($imagePath));
         }
     }
+
+    // public static function uploadPdf($pdf, $directory)
+    // {
+    //     $pdfName = uniqid().'.'.$pdf->getClientOriginalExtension();
+    //     $path = $pdf->storeAs($directory, $pdfName, 'public');
+    //     return 'storage/' . $path;
+    // }
 
     public static function uploadPdf($pdf, $directory)
     {
         $pdfName = uniqid().'.'.$pdf->getClientOriginalExtension();
-        $path = $pdf->storeAs($directory, $pdfName, 'public');
-        return 'storage/' . $path;
+        $pdf->move(public_path($directory), $pdfName);
+        $pdfPath = $directory.'/'.$pdfName;
+        return $pdfPath;
     }
+
+    // public static function deletePdf(string $pdfPath)
+    // {
+    //     if($pdfPath) {
+    //         $relativePath = str_replace('storage/', '', $pdfPath);
+    //         if(Storage::disk('public')->exists($relativePath)) {
+    //             Storage::disk('public')->delete($relativePath);
+    //         }
+    //     }
+    // }
 
     public static function deletePdf(string $pdfPath)
     {
-        if($pdfPath) {
-            $relativePath = str_replace('storage/', '', $pdfPath);
-            if(Storage::disk('public')->exists($relativePath)) {
-                Storage::disk('public')->delete($relativePath);
-            }
+        if ($pdfPath && File::exists(public_path($pdfPath))) {
+            File::delete(public_path($pdfPath));
         }
     }
 }
