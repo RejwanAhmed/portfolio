fffa86357573df9e002252688c862a50ae3b29d4
RejwanAhmed <rejwancse10@gmail.com>
2026-06-16 23:12:21 +0600

    js converted to ts

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -3,23 +3,24 @@ import './bootstrap';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createApp, h } from 'vue';
+import { createApp, h, DefineComponent } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy/src/js';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) =>
+    title: (title: string) => `${title} - ${appName}`,
+    resolve: (name: string) =>
         resolvePageComponent(
             `./Pages/${name}.vue`,
-            import.meta.glob('./Pages/**/*.vue'),
+            import.meta.glob<DefineComponent>('./Pages/**/*.vue'),
         ),
-    setup({ el, App, props, plugin }) {
-        return createApp({ render: () => h(App, props) })
+    setup({ el, App, props, plugin }: { el: Element; App: DefineComponent; props: any; plugin: any }) {
+        const app = createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
-            .mount(el);
+            .use(ZiggyVue);
+
+        app.mount(el);
     },
     progress: {
         color: '#4B5563',
